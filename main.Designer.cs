1fab235377675f2b86b0eb8b0a90cae310a8ae80
Александр Колотушкин <sampsamp961@gmail.com>
2020-07-02 09:36:47 +1000

    Завершил дизайн

--- main.Designer.cs
+++ main.Designer.cs
@@ -32,7 +32,8 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
             this.strip = new System.Windows.Forms.ToolStrip();
             this.tabs_combo = new System.Windows.Forms.ToolStripComboBox();
-            this.toolStripComboBox2 = new System.Windows.Forms.ToolStripComboBox();
+            this.deystv_combo = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
             this.dgvnav = new System.Windows.Forms.BindingNavigator(this.components);
             this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
@@ -47,18 +48,31 @@
             this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.dataGridView = new System.Windows.Forms.DataGridView();
-            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.strip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvnav)).BeginInit();
             this.dgvnav.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // strip
             // 
             this.strip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tabs_combo,
-            this.toolStripComboBox2,
+            this.deystv_combo,
             this.toolStripButton2});
             this.strip.Location = new System.Drawing.Point(0, 0);
             this.strip.Name = "strip";
@@ -79,20 +93,33 @@
             "Дисциплины",
             "Авторы",
             "Должности"});
+            this.tabs_combo.MaxDropDownItems = 10;
             this.tabs_combo.Name = "tabs_combo";
             this.tabs_combo.Size = new System.Drawing.Size(121, 25);
             this.tabs_combo.Text = "Таблица";
             this.tabs_combo.SelectedIndexChanged += new System.EventHandler(this.tabs_combo_SelectedIndexChanged);
             // 
-            // toolStripComboBox2
+            // deystv_combo
             // 
-            this.toolStripComboBox2.Items.AddRange(new object[] {
+            this.deystv_combo.Items.AddRange(new object[] {
             "Просмотр",
             "Редактирование"});
-            this.toolStripComboBox2.Name = "toolStripComboBox2";
-            this.toolStripComboBox2.Size = new System.Drawing.Size(121, 25);
-            this.toolStripComboBox2.Text = "Действие";
-            this.toolStripComboBox2.ToolTipText = "Выбор действия";
+            this.deystv_combo.Name = "deystv_combo";
+            this.deystv_combo.Size = new System.Drawing.Size(121, 25);
+            this.deystv_combo.Text = "Действие";
+            this.deystv_combo.ToolTipText = "Выбор действия";
+            this.deystv_combo.SelectedIndexChanged += new System.EventHandler(this.deystv_combo_SelectedIndexChanged);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
             // 
             // dgvnav
             // 
@@ -128,6 +155,7 @@
             // bindingNavigatorAddNewItem
             // 
             this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Enabled = false;
             this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
             this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
             this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
@@ -144,6 +172,7 @@
             // bindingNavigatorDeleteItem
             // 
             this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Enabled = false;
             this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
             this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
             this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
@@ -214,10 +243,14 @@
             // toolStripButton1
             // 
             this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Enabled = false;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
             this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton1.Name = "toolStripButton1";
             this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
             this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.ToolTipText = "Подтвердить добавление";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
             // 
             // dataGridView
             // 
@@ -229,16 +262,126 @@
             this.dataGridView.Size = new System.Drawing.Size(800, 394);
             this.dataGridView.TabIndex = 6;
             // 
-            // toolStripButton2
+            // panel1
             // 
-            this.toolStripButton2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
-            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton2.Text = "toolStripButton2";
-            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.textBox6);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(518, 242);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(282, 180);
+            this.panel1.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(128, 145);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "label5";
+            this.label5.Visible = false;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(128, 119);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "label6";
+            this.label6.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(128, 93);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(128, 67);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            this.label4.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(128, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
             // 
             // main
             // 
@@ -246,6 +389,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.strip);
             this.Controls.Add(this.dgvnav);
             this.Controls.Add(this.dataGridView);
@@ -260,6 +404,8 @@
             this.dgvnav.ResumeLayout(false);
             this.dgvnav.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -269,7 +415,7 @@
 
         private System.Windows.Forms.ToolStrip strip;
         private System.Windows.Forms.ToolStripComboBox tabs_combo;
-        private System.Windows.Forms.ToolStripComboBox toolStripComboBox2;
+        private System.Windows.Forms.ToolStripComboBox deystv_combo;
         private System.Windows.Forms.ToolStripButton toolStripButton2;
         private System.Windows.Forms.BindingNavigator dgvnav;
         private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
@@ -285,6 +431,19 @@
         private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
         private System.Windows.Forms.ToolStripButton toolStripButton1;
         private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- main.cs
+++ main.cs
@@ -17,33 +17,123 @@ namespace Libriray
         private OleDbConnection conn;
         private OleDbDataAdapter myDataAdapter;
         private DataSet myDataSet;
-        private BindingSource bindingSource;
+        BindingSource bs1 = new BindingSource();
+        Control tb;
+
+        const int Margin = 26;
+        const int tbstart_pos = 15;
+        int tb_pos = 0;
+
+        const int lb_start_pos = 18;
+        int lb_pos = 0;
+
+        const int lb_x_pos = 12;
+        const int tb_x_pos = 128;
+
+
         OleDbCommand myOleDbCommand;
-        Dictionary<string, string> selecQuerys = new Dictionary<string, string> {
+
+        Dictionary<string, string> selecQuerys = new Dictionary<string, string>
+        {
             { "pub", "SELECT pub.name AS [Название издания], author.name AS [Имя автора], author.last_n AS [Фамилия автора], pub_spec.name AS [Вид издания], discipline.name AS [Дисциплина] " +
                      "FROM discipline INNER JOIN (pub_spec INNER JOIN (author INNER JOIN pub ON author.id_author = pub.id_author) ON pub_spec.id_pub_spec = pub.id_pub_spec) ON discipline.id_discipline = pub.id_discipline;" },
 
-            { "give_pub", "SELECT rcpnt.name, rcpnt.last_n, pstions.name, pub.name, give_pub.num, give_pub.datetime " +
+            { "give_pub", "SELECT  rcpnt.last_n AS [Фамилия получателя],rcpnt.name AS [Имя получатяеля], pstions.name AS [Должность], pub.name AS [Издание] , give_pub.num AS [Количество], give_pub.datetime AS [Дата] " +
                 " FROM pstions INNER JOIN (pub INNER JOIN (rcpnt INNER JOIN give_pub ON rcpnt.id_rcpnt = give_pub.id_rcpnt) ON pub.id_pub = give_pub.id_pub) ON pstions.id_pstion = rcpnt.id_pstion;" },
-            { "return_pub", "SELECT rcpnt.last_n, rcpnt.name, pstions.name, pub.name, return_pub.num, return_pub.datetime " +
+            { "return_pub", "SELECT rcpnt.last_n AS [Фамилия получателя], rcpnt.name  AS [Имя получателя], pstions.name  AS [Должность], pub.name  AS [Издание], return_pub.num AS [Количество], return_pub.datetime  AS [Дата] " +
                 " FROM pstions INNER JOIN (pub INNER JOIN (rcpnt INNER JOIN return_pub ON rcpnt.id_rcpnt = return_pub.id_rcpnt) ON pub.id_pub = return_pub.id_pub) ON pstions.id_pstion = rcpnt.id_pstion;" },
-            { "rcpnt", "SELECT rcpnt.last_n, rcpnt.name, rcpnt.mid_n, pstions.name" +
+            { "rcpnt", "SELECT rcpnt.last_n AS [Фамилия], rcpnt.name AS [Имя], rcpnt.mid_n AS [Отчество], pstions.name AS [Должность]" +
                 " FROM pstions INNER JOIN rcpnt ON pstions.id_pstion = rcpnt.id_pstion;" },
-            { "num_of_pub", "SELECT pub.name, num_of_pub.num" +
+            { "num_of_pub", "SELECT pub.name AS [Издание], num_of_pub.num AS [Количество]" +
                 " FROM pub INNER JOIN num_of_pub ON pub.id_pub = num_of_pub.id_pub;" },
-            { "pub_spec", "SELECT pub_spec.name, type_of_pub.name" +
+            { "pub_spec", "SELECT pub_spec.name AS [Вид издания], type_of_pub.name AS [Тип издания]" +
                 " FROM type_of_pub INNER JOIN pub_spec ON type_of_pub.id_type_of_pub = pub_spec.id_type_of_pub;" },
-            { "type_of_pub", "SELECT type_of_pub.name" +
+            { "type_of_pub", "SELECT type_of_pub.name AS [Тип издания]" +
                 " FROM type_of_pub;" },
-            { "discipline", "SELECT discipline.name" +
+            { "discipline", "SELECT discipline.name AS [Дисциплина]" +
                 " FROM discipline;" },
-            { "author", "SELECT author.last_n, author.name, author.mid_n" +
+            { "author", "SELECT author.last_n AS [Фамилия], author.name AS [Имя], author.mid_n AS [Отчество]" +
                 " FROM author;" },
-            { "pstions", "SELECT pstions.name" +
+            { "pstions", "SELECT pstions.name AS [Должность]" +
                 " FROM pstions;" },
         };
 
-        string[] tabNames = new string[10] { "pub","give_pub","return_pub","rcpnt","num_of_pub","pub_sec","type_of_pub","discipline","author","pstions" };
+        Dictionary<string, string> comboQuerys = new Dictionary<string, string>
+        {
+            {"pub","" },
+            {"give_pub","" },
+            {"return_pub","" },
+            {"rcpnt","" },
+            {"num_of_pub","" },
+            {"pub_spec","" },
+            {"type_of_pub","" },
+            {"discipline","" },
+            {"author","" },
+            {"pstions","" }
+        };
+
+
+
+        Dictionary<string, Control[]> insertElements = new Dictionary<string, Control[]>
+        {
+            {"pub", new Control[] { new TextBox() {}, new ComboBox(), new ComboBox(), new ComboBox() } },
+            {"give_pub", new Control[] { new ComboBox(), new ComboBox(), new ComboBox(), new TextBox(), new DateTimePicker() } },
+            {"return_pub", new Control[] { new ComboBox(), new ComboBox(), new ComboBox(), new TextBox(), new DateTimePicker()} },
+            {"rcpnt", new Control[] { new TextBox(), new TextBox() { }, new TextBox(), new ComboBox() } },
+            {"num_of_pub", new Control[] { new ComboBox(), new TextBox()} },
+            {"pub_spec", new Control[] { new TextBox(), new ComboBox() {} } },
+            {"type_of_pub", new Control[] { new TextBox() } },
+            {"discipline", new Control[] { new TextBox()} },
+            {"author", new Control[] { new TextBox(), new TextBox() {}, new TextBox() } },
+            {"pstions", new Control[] { new TextBox()} }
+        };
+
+        /*Dictionary<string, string[]> insertElements = new Dictionary<string, string[]>
+        {
+            {"pub",new string[]{ "" } },
+            {"give_pub",new string[]{ } },
+            {"return_pub",new string[]{ } },
+            {"rcpnt",new string[]{ } },
+            {"num_of_pub",new string[]{ } },
+            {"pub_spec",new string[]{ } },
+            {"type_of_pub",new string[]{ } },
+            {"discipline",new string[]{ }},
+            {"author",new string[]{ }},
+            {"pstions", new string[]{ } }
+        };*/
+        
+
+
+        Dictionary<string, string[]> tabsCaption = new Dictionary<string, string[]>
+        {
+            {"pub", new string[] { "Название издания", "Автор", "Вид издания", "Дисциплина" } },
+            {"give_pub",new string[] { "Получатель", "Должность", "Издание", "Количество", "Дата"}},
+            {"return_pub",new string[] { "Получатель", "Должность", "Издание", "Количество", "Дата"} },
+            {"rcpnt",new string[] { "Фамилия", "Имя","Отчество", "Должность"}},
+            {"num_of_pub",new string[] {"Издание","Количество"}},
+            {"pub_spec",new string[] { "Вид издания", "Тип издания"}},
+            {"type_of_pub",new string[] { "Тип издания"} },
+            {"discipline",new string[] { "Дисциплина" }},
+            {"author",new string[] { "Фамилия", "Имя","Отчество"}},
+            {"pstions",new string[] { "Должность"}}
+        };
+
+
+        Dictionary<string, string> insertQuerys = new Dictionary<string, string>
+        {
+            {"pub","" },
+            {"give_pub","" },
+            {"return_pub","" },
+            {"rcpnt","" },
+            {"num_of_pub","" },
+            {"pub_spec","" },
+            {"type_of_pub","" },
+            {"discipline","" },
+            {"author","" },
+            {"pstions","" }
+        };
+
+        string[] tabNames = new string[10] { "pub","give_pub","return_pub","rcpnt","num_of_pub","pub_spec","type_of_pub","discipline","author","pstions" };
 
 
 
@@ -62,10 +152,14 @@ namespace Libriray
             Bitmap bitmap1 = Bitmap.FromHicon(SystemIcons.Question.Handle);
             toolStripButton2.Image = bitmap1;
 
+            //bitmap1 = Bitmap.FromResource();
+
             conn = new OleDbConnection(connectionString);
             myDataAdapter = new OleDbDataAdapter();
 
-            
+            panel1.Visible = false;
+
+            tabs_combo.SelectedIndex = 0;
 
             ViewTab("pub");
 
@@ -82,17 +176,85 @@ namespace Libriray
             myDataAdapter.SelectCommand = myOleDbCommand;
             myDataSet = new DataSet();
             myDataAdapter.Fill(myDataSet);
-            dataGridView.DataSource = myDataSet.Tables[0];
+            bs1 = new BindingSource(myDataSet, myDataSet.Tables[0].ToString());
+            dataGridView.DataSource = bs1;
             myDataAdapter.Update(myDataSet);
-
-            dgvnav.BindingSource = new BindingSource(myDataSet, myDataSet.Tables[0].ToString());
+            dgvnav.BindingSource = bs1;
 
             conn.Close();
         }
 
         private void tabs_combo_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (deystv_combo.SelectedIndex == 1)
+            {
+                deystv_combo_SelectedIndexChanged(sender, e);
+            }
             ViewTab(tabNames[tabs_combo.SelectedIndex]);
+        }
+
+
+        void InsertInTab(string who)
+        {
+
+        }
+
+        private void deystv_combo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (deystv_combo.SelectedIndex == 1)
+            {
+                panel1.Visible = true;
+                //Добавление записей на панель
+
+                lb_pos = lb_start_pos;
+                tb_pos = tbstart_pos;
+                panel1.Controls.Clear();
+
+                bindingNavigatorAddNewItem.Enabled = true;
+                bindingNavigatorDeleteItem.Enabled = true;
+                toolStripButton1.Enabled = true;
+
+                for (int i = 0; i < tabsCaption[tabNames[tabs_combo.SelectedIndex]].Length; i++)
+                {
+                    Label lbl = new Label()
+                    {
+                        Name = "lbl" + i,
+                        Location = new Point(lb_x_pos, lb_pos),
+                        Text = tabsCaption[tabNames[tabs_combo.SelectedIndex]][i]
+                    };
+
+                    try
+                    {
+                        tb = insertElements[tabNames[tabs_combo.SelectedIndex]][i];
+                        tb.Name = "tb" + i;
+                        tb.Location = new Point(tb_x_pos, tb_pos);
+                    }
+                    catch
+                    {
+
+                    }
+
+
+                    panel1.Controls.Add(lbl);
+                    panel1.Controls.Add(tb);
+
+                    lb_pos += Margin;
+                    tb_pos += Margin;
+                }
+            }
+            else
+            {
+                panel1.Visible = false;
+                bindingNavigatorAddNewItem.Enabled = false;
+                bindingNavigatorDeleteItem.Enabled = false;
+                toolStripButton1.Enabled = false;
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            InsertInTab(tabNames[tabs_combo.SelectedIndex]);
         }
     }
 }
