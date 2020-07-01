2ea8af24547bf120a8603961386468202874ac7d
Александр Колотушкин <sampsamp961@gmail.com>
2020-07-02 06:13:46 +1000

    Обновил

--- main.Designer.cs
+++ main.Designer.cs
@@ -30,76 +30,69 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.главноеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.strip = new System.Windows.Forms.ToolStrip();
+            this.tabs_combo = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripComboBox2 = new System.Windows.Forms.ToolStripComboBox();
             this.dgvnav = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
             this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
-            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
             this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
-            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.strip = new System.Windows.Forms.ToolStrip();
-            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.cправкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripComboBox2 = new System.Windows.Forms.ToolStripComboBox();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.Panel1.SuspendLayout();
-            this.splitContainer1.Panel2.SuspendLayout();
-            this.splitContainer1.SuspendLayout();
-            this.menuStrip1.SuspendLayout();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.strip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvnav)).BeginInit();
             this.dgvnav.SuspendLayout();
-            this.strip.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // splitContainer1
+            // strip
             // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
-            this.splitContainer1.Name = "splitContainer1";
+            this.strip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tabs_combo,
+            this.toolStripComboBox2,
+            this.toolStripButton2});
+            this.strip.Location = new System.Drawing.Point(0, 0);
+            this.strip.Name = "strip";
+            this.strip.Size = new System.Drawing.Size(800, 25);
+            this.strip.TabIndex = 5;
+            this.strip.Text = "toolStrip1";
             // 
-            // splitContainer1.Panel1
+            // tabs_combo
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.listBox1);
-            this.splitContainer1.Panel1.Controls.Add(this.comboBox1);
+            this.tabs_combo.Items.AddRange(new object[] {
+            "Издания",
+            "Выдача изданий",
+            "Возвращение изданий",
+            "Получатель",
+            "Количество изданий",
+            "Виды изданий",
+            "Типы изданий",
+            "Дисциплины",
+            "Авторы",
+            "Должности"});
+            this.tabs_combo.Name = "tabs_combo";
+            this.tabs_combo.Size = new System.Drawing.Size(121, 25);
+            this.tabs_combo.Text = "Таблица";
+            this.tabs_combo.SelectedIndexChanged += new System.EventHandler(this.tabs_combo_SelectedIndexChanged);
             // 
-            // splitContainer1.Panel2
+            // toolStripComboBox2
             // 
-            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
-            this.splitContainer1.Panel2.Controls.Add(this.strip);
-            this.splitContainer1.Panel2.Controls.Add(this.dgvnav);
-            this.splitContainer1.Size = new System.Drawing.Size(800, 426);
-            this.splitContainer1.SplitterDistance = 199;
-            this.splitContainer1.TabIndex = 2;
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.главноеToolStripMenuItem,
-            this.cправкаToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
-            this.menuStrip1.TabIndex = 3;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // главноеToolStripMenuItem
-            // 
-            this.главноеToolStripMenuItem.Name = "главноеToolStripMenuItem";
-            this.главноеToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
-            this.главноеToolStripMenuItem.Text = "Главное";
+            this.toolStripComboBox2.Items.AddRange(new object[] {
+            "Просмотр",
+            "Редактирование"});
+            this.toolStripComboBox2.Name = "toolStripComboBox2";
+            this.toolStripComboBox2.Size = new System.Drawing.Size(121, 25);
+            this.toolStripComboBox2.Text = "Действие";
+            this.toolStripComboBox2.ToolTipText = "Выбор действия";
             // 
             // dgvnav
             // 
@@ -121,16 +114,41 @@
             this.bindingNavigatorAddNewItem,
             this.bindingNavigatorDeleteItem,
             this.toolStripButton1});
-            this.dgvnav.Location = new System.Drawing.Point(0, 401);
+            this.dgvnav.Location = new System.Drawing.Point(0, 425);
             this.dgvnav.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
             this.dgvnav.MoveLastItem = this.bindingNavigatorMoveLastItem;
             this.dgvnav.MoveNextItem = this.bindingNavigatorMoveNextItem;
             this.dgvnav.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
             this.dgvnav.Name = "dgvnav";
             this.dgvnav.PositionItem = this.bindingNavigatorPositionItem;
-            this.dgvnav.Size = new System.Drawing.Size(597, 25);
-            this.dgvnav.TabIndex = 0;
+            this.dgvnav.Size = new System.Drawing.Size(800, 25);
+            this.dgvnav.TabIndex = 4;
             this.dgvnav.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Добавить";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(43, 22);
+            this.bindingNavigatorCountItem.Text = "для {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Общее число элементов";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorDeleteItem.Text = "Удалить";
             // 
             // bindingNavigatorMoveFirstItem
             // 
@@ -165,16 +183,9 @@
             this.bindingNavigatorPositionItem.Text = "0";
             this.bindingNavigatorPositionItem.ToolTipText = "Текущее положение";
             // 
-            // bindingNavigatorCountItem
-            // 
-            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
-            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(43, 22);
-            this.bindingNavigatorCountItem.Text = "для {0}";
-            this.bindingNavigatorCountItem.ToolTipText = "Общее число элементов";
-            // 
             // bindingNavigatorSeparator1
             // 
-            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator1";
             this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 25);
             // 
             // bindingNavigatorMoveNextItem
@@ -197,83 +208,8 @@
             // 
             // bindingNavigatorSeparator2
             // 
-            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
             this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 25);
-            // 
-            // bindingNavigatorAddNewItem
-            // 
-            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
-            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
-            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorAddNewItem.Text = "Добавить";
-            // 
-            // bindingNavigatorDeleteItem
-            // 
-            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
-            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
-            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorDeleteItem.Text = "Удалить";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 4);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(173, 21);
-            this.comboBox1.TabIndex = 0;
-            this.comboBox1.Text = "Вебор действия";
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 31);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(173, 368);
-            this.listBox1.TabIndex = 1;
-            // 
-            // strip
-            // 
-            this.strip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripComboBox1,
-            this.toolStripComboBox2});
-            this.strip.Location = new System.Drawing.Point(0, 0);
-            this.strip.Name = "strip";
-            this.strip.Size = new System.Drawing.Size(597, 25);
-            this.strip.TabIndex = 1;
-            this.strip.Text = "toolStrip1";
-            // 
-            // toolStripComboBox1
-            // 
-            this.toolStripComboBox1.Name = "toolStripComboBox1";
-            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 25);
-            this.toolStripComboBox1.Text = "Таблица";
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 31);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(591, 367);
-            this.dataGridView1.TabIndex = 2;
-            // 
-            // cправкаToolStripMenuItem
-            // 
-            this.cправкаToolStripMenuItem.Name = "cправкаToolStripMenuItem";
-            this.cправкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
-            this.cправкаToolStripMenuItem.Text = "Cправка";
-            // 
-            // toolStripComboBox2
-            // 
-            this.toolStripComboBox2.Items.AddRange(new object[] {
-            "Просмотр",
-            "Редактирование"});
-            this.toolStripComboBox2.Name = "toolStripComboBox2";
-            this.toolStripComboBox2.Size = new System.Drawing.Size(121, 25);
-            this.toolStripComboBox2.Text = "Действие";
             // 
             // toolStripButton1
             // 
@@ -283,29 +219,47 @@
             this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
             this.toolStripButton1.Text = "toolStripButton1";
             // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.Location = new System.Drawing.Point(0, 28);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.Size = new System.Drawing.Size(800, 394);
+            this.dataGridView.TabIndex = 6;
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
             // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.splitContainer1);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.Controls.Add(this.strip);
+            this.Controls.Add(this.dgvnav);
+            this.Controls.Add(this.dataGridView);
+            this.MaximizeBox = false;
             this.Name = "main";
+            this.ShowIcon = false;
             this.Text = "Library";
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            this.splitContainer1.Panel2.ResumeLayout(false);
-            this.splitContainer1.Panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
+            this.Load += new System.EventHandler(this.main_Load);
+            this.strip.ResumeLayout(false);
+            this.strip.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvnav)).EndInit();
             this.dgvnav.ResumeLayout(false);
             this.dgvnav.PerformLayout();
-            this.strip.ResumeLayout(false);
-            this.strip.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -313,12 +267,10 @@
 
         #endregion
 
-        private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.ToolStrip strip;
-        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripComboBox tabs_combo;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox2;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
         private System.Windows.Forms.BindingNavigator dgvnav;
         private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
         private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
@@ -331,11 +283,8 @@
         private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
         private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
         private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem главноеToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem cправкаToolStripMenuItem;
-        private System.Windows.Forms.ToolStripComboBox toolStripComboBox2;
         private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.DataGridView dataGridView;
     }
 }
 
--- main.cs
+++ main.cs
@@ -7,14 +7,92 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace Libriray
 {
     public partial class main : Form
     {
+        public static string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=er.mdb;";
+        private OleDbConnection conn;
+        private OleDbDataAdapter myDataAdapter;
+        private DataSet myDataSet;
+        private BindingSource bindingSource;
+        OleDbCommand myOleDbCommand;
+        Dictionary<string, string> selecQuerys = new Dictionary<string, string> {
+            { "pub", "SELECT pub.name AS [Название издания], author.name AS [Имя автора], author.last_n AS [Фамилия автора], pub_spec.name AS [Вид издания], discipline.name AS [Дисциплина] " +
+                     "FROM discipline INNER JOIN (pub_spec INNER JOIN (author INNER JOIN pub ON author.id_author = pub.id_author) ON pub_spec.id_pub_spec = pub.id_pub_spec) ON discipline.id_discipline = pub.id_discipline;" },
+
+            { "give_pub", "SELECT rcpnt.name, rcpnt.last_n, pstions.name, pub.name, give_pub.num, give_pub.datetime " +
+                " FROM pstions INNER JOIN (pub INNER JOIN (rcpnt INNER JOIN give_pub ON rcpnt.id_rcpnt = give_pub.id_rcpnt) ON pub.id_pub = give_pub.id_pub) ON pstions.id_pstion = rcpnt.id_pstion;" },
+            { "return_pub", "SELECT rcpnt.last_n, rcpnt.name, pstions.name, pub.name, return_pub.num, return_pub.datetime " +
+                " FROM pstions INNER JOIN (pub INNER JOIN (rcpnt INNER JOIN return_pub ON rcpnt.id_rcpnt = return_pub.id_rcpnt) ON pub.id_pub = return_pub.id_pub) ON pstions.id_pstion = rcpnt.id_pstion;" },
+            { "rcpnt", "SELECT rcpnt.last_n, rcpnt.name, rcpnt.mid_n, pstions.name" +
+                " FROM pstions INNER JOIN rcpnt ON pstions.id_pstion = rcpnt.id_pstion;" },
+            { "num_of_pub", "SELECT pub.name, num_of_pub.num" +
+                " FROM pub INNER JOIN num_of_pub ON pub.id_pub = num_of_pub.id_pub;" },
+            { "pub_spec", "SELECT pub_spec.name, type_of_pub.name" +
+                " FROM type_of_pub INNER JOIN pub_spec ON type_of_pub.id_type_of_pub = pub_spec.id_type_of_pub;" },
+            { "type_of_pub", "SELECT type_of_pub.name" +
+                " FROM type_of_pub;" },
+            { "discipline", "SELECT discipline.name" +
+                " FROM discipline;" },
+            { "author", "SELECT author.last_n, author.name, author.mid_n" +
+                " FROM author;" },
+            { "pstions", "SELECT pstions.name" +
+                " FROM pstions;" },
+        };
+
+        string[] tabNames = new string[10] { "pub","give_pub","return_pub","rcpnt","num_of_pub","pub_sec","type_of_pub","discipline","author","pstions" };
+
+
+
         public main()
         {
             InitializeComponent();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            Bitmap bitmap1 = Bitmap.FromHicon(SystemIcons.Question.Handle);
+            toolStripButton2.Image = bitmap1;
+
+            conn = new OleDbConnection(connectionString);
+            myDataAdapter = new OleDbDataAdapter();
+
+            
+
+            ViewTab("pub");
+
+            
+        }
+
+
+        void ViewTab(string who)
+        {
+
+
+            conn.Open();
+            myOleDbCommand = new OleDbCommand(selecQuerys[who], conn);
+            myDataAdapter.SelectCommand = myOleDbCommand;
+            myDataSet = new DataSet();
+            myDataAdapter.Fill(myDataSet);
+            dataGridView.DataSource = myDataSet.Tables[0];
+            myDataAdapter.Update(myDataSet);
+
+            dgvnav.BindingSource = new BindingSource(myDataSet, myDataSet.Tables[0].ToString());
+
+            conn.Close();
+        }
+
+        private void tabs_combo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ViewTab(tabNames[tabs_combo.SelectedIndex]);
         }
     }
 }
